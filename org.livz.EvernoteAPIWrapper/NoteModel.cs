533fd6deeb17e16746a5b0c93a3c40b84630deb8
Steven Livingstone-Perez <weblivz@hotmail.com>
2013-06-05 07:34:12 +0100

    Initial commit.

---
+++ org.livz.EvernoteAPIWrapper/NoteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.livz.EvernoteAPIWrapper
+{
+    public class NoteModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public bool IsPublic { get; set; }
+        public System.DateTime DateCreated { get; set; }
+        public System.DateTime DateModified { get; set; }
+    }
+}
